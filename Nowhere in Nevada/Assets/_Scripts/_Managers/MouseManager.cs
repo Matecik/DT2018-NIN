5d23bfd1b734900ea0a9fa396d6bc47743a26be3
@Max <ritcma2020@SICT-9.local>
2018-09-06 09:35:50 +0800

    Starting
    Start of work

---
+++ Nowhere in Nevada/Assets/_Scripts/_Managers/MouseManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            Debug.Log(hit.collider.gameObject);
+        }
+        */
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+        }
+    }
+}
